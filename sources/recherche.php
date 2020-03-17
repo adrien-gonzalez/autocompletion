984852a263bc8aecfd37819f1c5b11d6e84a70ba
adrien-gonzalez <adrien.gonzalez@laplateforme.io>
2020-03-17 20:18:49 +0100

    Sources

--- sources/recherche.php
+++ sources/recherche.php
@@ -2,7 +2,7 @@
 <head>
 	<meta charset="utf-8">
 	<link href="../style.css" rel="stylesheet">
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
+	<script type="text/javascript" src="../../js/jquery-3.4.1.min.js"></script>
 		<!-- MON SCRIPT -->
 		<script type="text/javascript" src="../../js/script.js"></script>
 	<title>resultat de recherche</title>
