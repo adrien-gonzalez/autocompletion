db53e8c80ff7bb0b319d8f97650e6e249c802d50
adrien-gonzalez <adrien.gonzalez@laplateforme.io>
2020-03-17 10:36:12 +0100

    Accueil

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<title>Accueil</title>
+		<meta charset="UTF-8">
+		<link href="sources/style.css" rel="stylesheet">
+		<!-- JQUERY -->
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
+		<!-- MON SCRIPT -->
+		<script type="text/javascript" src="js/script.js"></script>
+	</head>
+<body class="accueil" id="pokemon">
+	<?php include("sources/header.php");?>
+
+<div class="form_research">
+	<form class="research" method="get">
+		<input placeholder="Tapez votre recherche" type="text" name="search" id="search" onkeypress="refuserToucheEntree(event);">
+		<input type="button" name="recherche" id="recherche" value="Rechercher">
+	</form>
+</div>
+	
+
+</body>
+</html>
