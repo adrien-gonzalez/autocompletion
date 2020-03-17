c444410773fcce8f49130345533014f65d20a788
adrien-gonzalez <adrien.gonzalez@laplateforme.io>
2020-03-17 09:05:14 +0100

    Sources

---
+++ sources/header.php
@@ -0,0 +1,10 @@
+<header>
+	<ul>
+		<li>
+			<form method="post" action="">
+				<input type="text" name="mot_cle_barre" id="mot_cle_barre">
+				<input type="button" name="recherche_barre" id="recherche_barre" value="Rechercher">
+			</form>
+		</li>
+	</ul>
+</header>
