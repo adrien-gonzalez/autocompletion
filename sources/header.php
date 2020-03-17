18f6f40e95ba933842d7c0f7ca7d61d97a5e21c2
adrien-gonzalez <adrien.gonzalez@laplateforme.io>
2020-03-17 10:35:58 +0100

    Sources

--- sources/header.php
+++ sources/header.php
@@ -3,12 +3,12 @@
 
 	<ul>
 		<li>
-			<form method="post" action="">
+			<form method="post">
 				<input type="text" name="search_barre" id="search_barre" placeholder="Tapez votre recherche" onkeypress="refuserToucheEntree(event);">
 				<input type="button" name="recherche_barre" id="recherche_barre" value="Rechercher" >
 			</form>
 		</li>
 	</ul>
-	<img width="200px" src="../img/Pokemon_logo.png">
+	<img width="200px" src="img/Pokemon_logo.png">
 	</div>
 </header>
