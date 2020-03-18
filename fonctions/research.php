bc9cce7d9acc80cc62408ed06ff74f0b577c27e8
adrien-gonzalez <adrien.gonzalez@laplateforme.io>
2020-03-18 14:17:15 +0100

    Fonctions

--- fonctions/research.php
+++ fonctions/research.php
@@ -5,7 +5,7 @@ include("config.php");
 $search=$_GET['search'];
 
 
-$research="SELECT id, nom_pok FROM pokemon WHERE nom_pok LIKE '%".$search."%' or type1_pok LIKE '%".$search."%' or  type2_pok LIKE '%".$search."%'";
+$research="SELECT id, nom_pok FROM pokemon WHERE nom_pok LIKE '%".$search."%' or type1_pok LIKE '%".$search."%' or  type2_pok LIKE '%".$search."%' ORDER BY id ASC";
 $resultat_research=mysqli_query($base, $research);
 $element=mysqli_num_rows($resultat_research);
 
