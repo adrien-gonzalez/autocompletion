677ba6d6759d8ebf91b95307241448fbc2679ab5
olivier-crozet <olivier.crozet@laplateforme.io>
2020-03-19 19:28:19 +0100

    element ok a tester (barre de recherche)

--- sources/element.php
+++ sources/element.php
@@ -2,43 +2,50 @@
 <head>
 	<meta charset="utf-8">
 	<link href="../style.css" rel="stylesheet">
+	<link href="../element.css" rel="stylesheet">
 	<script type="text/javascript" src="../../js/jquery-3.4.1.min.js"></script>
 		<!-- MON SCRIPT -->
 		<script type="text/javascript" src="../../js/script.js"></script>
 	<title>resultat de recherche</title>
 </head>
-<body id="pokemon">
+<body id="pokemon" class="oc-body-element">
 	<!--////////////////////////////////////////////////HEADER/////////////////////////////////////////////////////-->
-	   	<?php include("header.php");?>
+	   	<?php include("header.php");
+
+	   	 //////////////////////////////////////:BDD EN PDO/////////////////////////////////////////////////////:
+	   	     $bdd = new PDO('mysql:host=localhost;dbname=autocompletion', 'root', '');    
+	   	     ////////////////////////:recuperation de id de url et securation ///////////////////////////////////////////:
+	   	     $idget =htmlspecialchars($_GET['id']) ;
+	   	       	        
+	   	  //////////////////////////////REQUETTE////////////////////////////////////////////////////////
+	   	     $articlesPok = $bdd->query("SELECT * FROM pokemon WHERE id = \"$_GET[id]\" ");	   	     
+	   	     //////////////////////////////////tableau du pokemon //////////////////////////////////////:
+	   	       $resultElement = $articlesPok->fetch();
+	   	?>
 <!--////////////////////////////////////////////////////MAIN/////////////////////////////////////////////::-->
-<main>
-	 <h1>Resultat de Recherche</h1>
+<main >
+	 
 	   	     <?php
 	   	     //////////////////////////////////////:BDD EN PDO/////////////////////////////////////////////////////:
-	   	     $bdd = new PDO('mysql:host=localhost;dbname=autocompletion', 'root', '');
-	   	     //////////////////////////////REQUETTE////////////////////////////////////////////////////////
-	   	     $articlesPok = $bdd->query("SELECT * FROM pokemon WHERE id = \"$_GET[id]\" ");
+	   	     $bdd = new PDO('mysql:host=localhost;dbname=autocompletion', 'root', '');    
 	   	     ////////////////////////:recuperation de id de url et securation ///////////////////////////////////////////:
-	   	     $idget =htmlspecialchars($_GET['id']) ;	   	     
-	   	     //////////////////////////////////tableau du pokemon //////////////////////////////////////:
-	   	       $resultElement = $articlesPok->fetch();   	        
+	   	     $idget =htmlspecialchars($_GET['id']) ;
+	   	       	        
 	   	   ?>
-	   	     <ul>
-	   	     	 <?php   	   
+	   	     <section class="oc-section-element">
+	   	        <h1><?php echo $resultElement[1] ?></h1>
+	   	     	 <?php   	
 	   	     	 ///////////////////////affichage des element du tableau du pokemon////////////////////////  	
-	   	     	  ?><li>Nom :<?= $resultElement[1] ?></li>
-	   	     	     	<li>type 1 :<?= $resultElement[2] ?></li>
-	   	     	     	<li>Type 2 :<?= $resultElement[3] ?></li>
-	   	     	     	<li>HP :<?= $resultElement[4] ?></li>
-	   	     	     	<li>Attaque :<?= $resultElement[5] ?></li>
-	   	     	     	<li>Defense :<?= $resultElement[6] ?></li>
-	   	     	     	<li> Sp attaque :<?= $resultElement[7] ?></li><br>
-	   	     	     	<li>Sp_Defense :<?= $resultElement[8] ?></li>
-	   	     	     	<li>Vitesse :<?= $resultElement[9] ?></li>
-	   	     	    <?php 
-	   	     	    ;   
-	   	     	 ?>
-	   	     </ul> 
+	   	     	   ?>
+	   	     	     	<li>type 1 <?= $resultElement[2] ?></li>
+	   	     	     	<li>Type 2 <?= $resultElement[3] ?></li>
+	   	     	     	<li>HP <?= $resultElement[4] ?></li>
+	   	     	     	<li>Attaque <?= $resultElement[5] ?></li>
+	   	     	     	<li>Defense <?= $resultElement[6] ?></li>
+	   	     	     	<li> Sp attaque <?= $resultElement[7] ?></li>
+	   	     	     	<li>Sp_Defense <?= $resultElement[8] ?></li>
+	   	     	     	<li>Vitesse <?= $resultElement[9] ?></li>
+	   	     </section> 
 </main>	
 </body>
 </html>
