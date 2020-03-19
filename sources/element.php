9cbf8b30c40cb762e142f382d81e78150347e737
olivier-crozet <olivier.crozet@laplateforme.io>
2020-03-19 17:19:40 +0100

    resolution probleme id

--- sources/element.php
+++ sources/element.php
@@ -20,43 +20,43 @@
 	   	     //////////////////////////////////////:BDD/////////////////////////////////////////////////////:
 	   	   //  $bdd = new PDO('mysql:host=127.0.0.1;dbname=autocompletion;charset=utf8','root',' ');
 	   	     $bdd = new PDO('mysql:host=localhost;dbname=autocompletion', 'root', '');
+	$articlesPok = $bdd->query("SELECT * FROM pokemon WHERE id = \"$_GET[id]\" ");
 
-
-	   	     $articlesPok = $bdd->query('SELECT * FROM pokemon ORDER BY id DESC ');
+	   	  //   $articlesPok = $bdd->query('SELECT * FROM pokemon ORDER BY id DESC ');
 	   	     	 $idget = $_GET['id'];
-
+	   	     	 var_dump($idget);
 	   	       if(isset($_GET['search']) && !empty($_GET['search'])) 
 	   	       {
 	   	       	$search = htmlspecialchars($_GET['search']);
 	
-	   	       	$articlesPok = $bdd->query('SELECT * FROM pokemon WHERE id = $idget ');
+	   	       
 
 	   	       	//$articlesPok = $bdd->query('SELECT * FROM pokemon WHERE nom_pok like"%'.$search.'%" ORDER BY id DESC ');
 	   	       }
-
+	   	       echo "string";
 	   	       $resultElement = $articlesPok->fetch();
 	   	      
-	   	         ;
+	   	        
 	   	   ?>
 
 	   	     <ul>
 	   	     	 <?php
-	   	     
 
 	   	     	
-	   	     	  ?><li>Nom :<?= $resultElement[0] ?></li>
-	   	     	     	<li>type 1 :<?= $resultElement[1] ?></li>
-	   	     	     	<li>Type 2 :<?= $resultElement[2] ?></li>
-	   	     	     	<li>HP :<?= $resultElement[3] ?></li>
-	   	     	     	<li>Attaque :<?= $resultElement[4] ?></li>
-	   	     	     	<li>Defense :<?= $resultElement[5] ?></li>
-	   	     	     	<li>Sp_Attaque :<?= $resultElement[6] ?></li>
-	   	     	     	<li>Sp_Defense :<?= $resultElement[7] ?></li><br>
-	   	     	     	
+	   	     	
+	   	     	  ?><li>Nom :<?= $resultElement[1] ?></li>
+	   	     	     	<li>type 1 :<?= $resultElement[2] ?></li>
+	   	     	     	<li>Type 2 :<?= $resultElement[3] ?></li>
+	   	     	     	<li>HP :<?= $resultElement[4] ?></li>
+	   	     	     	<li>Attaque :<?= $resultElement[5] ?></li>
+	   	     	     	<li>Defense :<?= $resultElement[6] ?></li>
+	   	     	     	<li> Sp attaque :<?= $resultElement[7] ?></li><br>
+	   	     	     	<li>Sp_Defense :<?= $resultElement[8] ?></li>
+	   	     	     	<li>Vitesse :<?= $resultElement[9] ?></li>
 	   	     	    <?php 
 	   	     	    ;   
 	   	     	 ?>
-	   	     </ul> -->
+	   	     </ul> 
 </main>
 	
 </body>
